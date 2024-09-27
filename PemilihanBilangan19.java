b26f1625a877a7931c1bd27203e875bf20945cff
PraNAtaP <pranatapu08@gmail.com>
2024-09-27 07:43:04 +0700

    halo

---
+++ PemilihanBilangan19.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PemilihanBilangan19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Masukan Angka : ");
+        int angka = sc.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Angka" + angka + "Adalah bilangan genap");
+        } else {
+            System.out.println("Angka" + angka + "Adalah bilangan ganjil");
+
+        }
+    }
+    }
