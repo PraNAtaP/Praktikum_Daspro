9d65d50bc2492cde70a99457839fb0602718e024
PraNAtaP <pranatapu08@gmail.com>
2024-09-27 10:10:48 +0700

    ini Kafe Keanggotaan

---
+++ Kafe19.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Kafe19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String menu;
+        char ukuranCup;
+        int jumlah;
+        boolean keanggotaan;
+
+        System.out.println("Masukan menu: ");
+        menu = sc.nextLine();
+        System.out.println("Masukkan ukuran cup: ");
+        ukuranCup = sc.next().charAt(0);
+        System.out.println("Masukkan jumlah: ");
+        jumlah = sc.nextInt();
+        System.out.println("Masukkan keanggotaan (true/false): ");
+        keanggotaan = sc.nextBoolean();
+
+        double hargaMenu = 0;
+
+        switch (menu.toLowerCase()) {
+            case "kopi":
+                hargaMenu = 12000;
+                break;
+            case "teh":
+                hargaMenu = 7000;
+                break;
+            case "coklat":
+                hargaMenu = 20000;
+                break;
+        
+        }
+        double totalHarga = hargaMenu * jumlah;
+
+        switch (ukuranCup) {
+            case 'S':
+                break;
+            case 'M':
+                break;
+            case 'L':
+                break;
+            
+        }
+        double diskon = keanggotaan ? 0.1 : 0;
+        double nominalBayar = totalHarga - (diskon * totalHarga);
+
+        System.out.println("Item pembelian: " + jumlah + " " + menu + " dengan ukuran cup " + ukuranCup);
+        System.out.println("Nominal Bayar: " + nominalBayar);
+    }
+}
